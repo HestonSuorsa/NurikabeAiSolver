9372e4fdb73f917d62f3cf4dfff32a2e236ee7eb
Heston P Suorsa <SuorsaHP19@gcc.edu>
2022-03-17 20:47:38 -0400

    Initial commit

---
+++ src/Board.java
@@ -0,0 +1,16 @@
+public class Board {
+    String[][] board;
+
+    public Boolean lakeExists() {
+        // TODO: Check if any lakes create 2x2
+        return false;
+    }
+    public Boolean pondExists() {
+        // TODO: Make sure water can be connected
+        return false;
+    }
+    public Boolean checkIslands() {
+        // TODO: Check if islands are no more than their max number
+        return false;
+    }
+}
