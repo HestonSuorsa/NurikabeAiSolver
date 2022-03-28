62a38cc4d0cacf4d7439bdaa7b7d76dbfeecc84f
barnesge19 <barnesge19@gcc.edu>
2022-03-28 19:14:05 -0400

    Added 10 by 10 board

--- src/Main.java
+++ src/Main.java
@@ -9,7 +9,7 @@ public class Main {
         // TODO: Run AI implementations and time them
         Scanner scan = null;
         try {
-            File f = new File("5x5_1.txt");
+            File f = new File("10x10_1.txt");
             scan = new Scanner(f);
         } catch (FileNotFoundException e) {
             System.out.println("File not found");
