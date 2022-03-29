0b9070a9116cd031e867550b5c5cbc2bb1387965
barnesge19 <barnesge19@gcc.edu>
2022-03-28 21:53:31 -0400

    errors but progress!

--- src/Main.java
+++ src/Main.java
@@ -9,7 +9,7 @@ public class Main {
         // TODO: Run AI implementations and time them
         Scanner scan = null;
         try {
-            File f = new File("10x10_1.txt");
+            File f = new File("3x3_1.txt");
             scan = new Scanner(f);
         } catch (FileNotFoundException e) {
             System.out.println("File not found");
@@ -22,25 +22,32 @@ public class Main {
         int numIslands = scan.nextInt();
 
         //Initialize Board
-        Board board = new Board(height, width);
+        Board board1 = new Board(height, width, true);
+
 
         //Add Origin islands
         for (int i = 0; i < numIslands; i++) {
             int size = scan.nextInt();
             int row = scan.nextInt();
             int col = scan.nextInt();
-            Cell curCell = board.getCell(row,col);
+            Cell curCell = board1.getCell(row,col);
             curCell.setIsOrigin(true);
             curCell.setIsLand();
             curCell.setIslandSize(size);
         }
+        //Clone board before DFS
+        Board board2 = board1.clone();
 
         //Now the board is completely initialized and dfs can happen
 
         //This breaks bc we aren't done yet lol
-        DFS_NurikabeAI dfs = new DFS_NurikabeAI(board, numIslands);
+//        DFS_NurikabeAI dfs = new DFS_NurikabeAI(board1, numIslands);
+//
+//        System.out.println(dfs.run());
+//        System.out.println("Nodes visited: " + dfs.nodesVisited);
 
-        System.out.println(dfs.run());
-        System.out.println("Nodes visited: " + dfs.nodesVisited);
+        MostConstrained_NurikabeAI mc = new MostConstrained_NurikabeAI(board2, numIslands);
+        mc.run();
+        System.out.println(board2);
     }
 }
